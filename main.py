27d290f314cd3db5fb36de7292e136011e4ab0c1
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2024-12-21 18:54:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 
 cap=cv2.VideoCapture(0) #0 for default camera 1 for external camera
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3,640)
+cap.set(4,480)
 
 imgBackground= cv2.imread('resources/background.png')
 
