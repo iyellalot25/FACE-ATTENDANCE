b856bd5760bb2428387c02c362a6687ef36dd76f
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2024-12-21 19:37:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 cap=cv2.VideoCapture(0) #0 for default camera 1 for external camera
 cap.set(3,640)
@@ -6,9 +7,17 @@ cap.set(4,480)
 
 imgBackground= cv2.imread('resources/background.png')
 
+folderModePath="resources/modes"
+imgModeList=[]
+for path in os.listdir(folderModePath):
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
+
 while True:
     success, img= cap.read()
-    cv2.imshow("Webcam",img)
+
+    imgBackground[162:162+480,55:55+640]= img #for overlay
+
+    #cv2.imshow("Webcam",img) #for display webcam feed seperately
     cv2.imshow("Face Attendance", imgBackground)
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
         break
