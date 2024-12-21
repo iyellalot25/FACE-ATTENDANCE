466531c0526be04ec7cc900e7eae4eb8d452156b
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2024-12-21 20:48:06 +0530

    Encoding complete

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -17,4 +17,16 @@ studentids=[]
 for path in os.listdir(folderPath):
     imgList.append(cv2.imread(os.path.join(folderPath,path)))
     studentids.append(extract_numbers(path))
-print(studentids)
+
+def findEncodings(imagesList):
+    encodeList=[]
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode=face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+print("Encoding initialised...")
+encodeListKnown=findEncodings(imgList)
+encodeListKnownWithIds=[encodeListKnown,studentids]
+print("Encoding Complete")
